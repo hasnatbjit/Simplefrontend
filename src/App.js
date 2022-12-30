289ed7803699880ed39979de6eb0c8ac1bd2557c
mdabuhasnat <fazleyrabby19@gmail.com>
2022-12-30 20:35:32 +0600

    Initial CI/CD for frontend

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Hear
+          Learn React
         </a>
       </header>
     </div>
